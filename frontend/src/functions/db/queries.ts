f7fdc99affa9951d8142ad540897381feef64f92
ccrawford4 <calumcrawford9@gmail.com>
2025-04-04 22:06:56 -0700

    create query page

--- frontend/src/functions/db/queries.ts
+++ frontend/src/functions/db/queries.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { PrismaClient } from '@prisma/client'
+import { Course } from '../../../generated/prisma';
 
 const prisma = new PrismaClient()
 
-export async function getAllCourses() {
+export async function getAllCourses(): Promise<Course[]> {
   const courses = await prisma.course.findMany();
   return courses;
 }
