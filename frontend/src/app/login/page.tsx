f11f76a18a6b033ace2baef193806ae6dfc4accf
ccrawford4 <calumcrawford9@gmail.com>
2025-04-05 19:33:22 -0700

    auth somewhat working

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    console.log("Session: ", session);
+    if (status === "authenticated") {
+      console.log("Already logged in. Redirecting to home page...");
+      router.push("/");
+    }
+  }, [status, session, router]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+}
